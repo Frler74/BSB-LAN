07d0472fae8ad8f315ee7de735a8dfe560fdb6e5
Franck <frler74@gmail.com>
2026-08-08 13:22:06 +0200

    Remove OTA test marker, OTA confirmed working

--- BSB_LAN/BSB_LAN_custom_setup.h
+++ BSB_LAN/BSB_LAN_custom_setup.h
@@ -12,7 +12,6 @@ water_last_save_ms = millis();
 
 custom_floats[0] = 0;                   // débit L/min (param 20700)
 custom_floats[1] = water_total_liters;  // volume total L (param 20701)
-custom_floats[2] = 12345;               // marqueur de test OTA (param 20702) -- à retirer une fois l'upload confirmé
 
 attachInterrupt(digitalPinToInterrupt(WATER_METER_PIN), water_meter_isr, CHANGE);
 printFmtToDebug("Water meter: CHANGE interrupt attached on GPIO%d, idle pin_state=%d (should be 1/HIGH if wiring OK)\r\n", WATER_METER_PIN, digitalRead(WATER_METER_PIN));
